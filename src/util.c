jlanza/umip

commit 7e3b3b7926f138479e4d0bbe6e1658077b513f14

Rewrite and move csum() to util module.

src/util.c
@@ -1,0 +1,71 @@
+/*
+ * $Id: icmp6.c 1.42 06/05/06 15:15:47+03:00 anttit@tcs.hut.fi $
+ *
+ * This file is part of the MIPL Mobile IPv6 for Linux.
+ *
+ * Authors: Antti Tuominen <anttit@tcs.hut.fi>
+ *          Ville Nuorvala <vnuorval@tcs.hut.fi>
+ *
+ * Copyright 2003-2005 Go-Core Project
+ * Copyright 2003-2006 Helsinki University of Technology
+ *
+ * MIPL Mobile IPv6 for Linux is free software; you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; version 2 of
+ * the License.
+ *
+ * MIPL Mobile IPv6 for Linux is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MIPL Mobile IPv6 for Linux; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307 USA.
+ */
+
+#include "util.h"
+
+/* Adapted from RFC 1071 "C" Implementation Example */
+uint16_t in6_cksum(const struct in6_addr *src, const struct in6_addr *dst,
+		   const void *data, socklen_t datalen, uint8_t nh)
+{
+	struct _phdr {
+		struct in6_addr src;
+		struct in6_addr dst;
+		uint32_t plen;
+		uint8_t reserved[3];
+		uint8_t nxt;
+	} phdr;
+
+	register unsigned long sum = 0;
+	socklen_t count;
+	uint16_t *addr;
+	int i;
+
+	/* Prepare pseudo header for csum */
+	memset(&phdr, 0, sizeof(phdr));
+	phdr.dst = *src;
+	phdr.dst = *dst;
+	phdr.plen = htonl(datalen);
+	phdr.nxt = nh;
+
+	/* caller must make sure datalen is even */
+	addr = (uint16_t *)&phdr;
+	for (i = 0; i < 20; i++)
+		sum += *addr++;
+
+	count = datalen;
+	addr = (uint16_t *)data;
+
+        while (count > 1) {
+		sum += *(addr++);
+		count -= 2;
+	}
+
+	while (sum >> 16)
+		sum = (sum & 0xffff) + (sum >> 16);
+
+	return (uint16_t)~sum;
+}

src/Makefile.am
@@ -25,7 +25,7 @@ mip6d_SOURCES = pmgr.c pmgr.h defpath.h \
 		gram.y scan.l \
 		tqueue.c tqueue.h \
 		icmp6.c icmp6.h \
-		list.h util.h \
+		list.h util.c util.h \
 		hash.c hash.h \
 		debug.c debug.h \
 		dhaad_ha.c dhaad_ha.h \

src/ndisc.c
@@ -204,34 +204,6 @@ static int nd_get_l2addr(int ifindex, uint8_t *addr)
 	return res;
 }
 
-/* Adapted from RFC 1071 "C" Implementation Example */
-static uint16_t csum(const void *phdr, const void *data, socklen_t datalen)
-{
-	register unsigned long sum = 0;
-	socklen_t count;
-	uint16_t *addr;
-	int i;
-
-	/* caller must make sure datalen is even */
-
-	addr = (uint16_t *)phdr;
-	for (i = 0; i < 20; i++)
-		sum += *addr++;
-
-	count = datalen;
-	addr = (uint16_t *)data;
-
-        while (count > 1) {
-		sum += *(addr++);
-		count -= 2;
-	}
-
-	while (sum >> 16)
-		sum = (sum & 0xffff) + (sum >> 16);
-
-	return (uint16_t)~sum;
-}
-
 /* Linearize 'iov' of 'iovlen' elements in 'dst' buffer of available size
  * 'dstlen'. The number of bytes written to dst is returned in 'written'
  * if everything went ok. 0 is returned on success, -1 on error. */
@@ -271,14 +243,6 @@ static int ndisc_send_unspec(int oif, const struct in6_addr *dest,
 			     uint8_t *hdr, int hdrlen, struct iovec *optv,
 			     size_t optvlen)
 {
-	struct _phdr {
-		struct in6_addr src;
-		struct in6_addr dst;
-		uint32_t plen;
-		uint8_t reserved[3];
-		uint8_t nxt;
-	} phdr;
-
 	struct {
 		struct ip6_hdr ip;
 		struct icmp6_hdr icmp;
@@ -329,17 +293,11 @@ static int ndisc_send_unspec(int oif, const struct in6_addr *dest,
 	frame.ip.ip6_plen = htons(datalen);
 	frame.ip.ip6_nxt = IPPROTO_ICMPV6;
 	frame.ip.ip6_hlim = 255;
-	frame.ip.ip6_dst = dst.sin6_addr;
+	frame.ip.ip6_dst = *dest;
 	/* all other fields are already set to zero */
 
-	/* Prepare pseudo header for csum */
-	memset(&phdr, 0, sizeof(phdr));
-	phdr.dst = dst.sin6_addr;
-	phdr.plen = htonl(datalen);
-	phdr.nxt = IPPROTO_ICMPV6;
-
-	/* Fill in remaining ICMP header fields */
-	frame.icmp.icmp6_cksum = csum(&phdr, &frame.icmp, datalen);
+	frame.icmp.icmp6_cksum = in6_cksum(&in6addr_any, dest, &frame.icmp,
+					   datalen, IPPROTO_ICMPV6);
 
 	iov.iov_base = &frame;
 	iov.iov_len = sizeof(frame.ip) + datalen;

src/util.h
@@ -207,6 +207,9 @@ static inline long max(long a, long b)
 	return (a > b) ? a : b;
 }
 
+uint16_t in6_cksum(const struct in6_addr *src, const struct in6_addr *dst,
+		   const void *data, socklen_t datalen, uint8_t nh);
+
 /*
  * XXX: These may be missing on kernel header because either kernel is not
  * ready or should be removed since kernel will never support it.

commit 7112dc5641af3d8082de71aa5415850a57543977

Fix typo in in6_cksum() preventing source address to be considered
The typo was introduced in 7e3b3b79 (Rewrite and move csum() to util module).

It remained unnoticed probably because in6_cksum() is only used by
ndisc_send_unspec(), which *always* passes the unspecified address as
source. Due to the memset() at the beginning of in6_cksum(), the
address used in the pseudo header was always the unspecified address.

Reported-by: Steve Chen <schen@mvista.com>
Signed-off-by: Arnaud Ebalard <arno@natisbad.org>

src/util.c
@@ -46,7 +46,7 @@ uint16_t in6_cksum(const struct in6_addr *src, const struct in6_addr *dst,
 
 	/* Prepare pseudo header for csum */
 	memset(&phdr, 0, sizeof(phdr));
-	phdr.dst = *src;
+	phdr.src = *src;
 	phdr.dst = *dst;
 	phdr.plen = htonl(datalen);
 	phdr.nxt = nh;