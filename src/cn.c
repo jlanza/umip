jlanza/umip

commit 36c929e1de9d3da40cc607e1b2bd73eb34b91d2d

Extract CN and HA checks of mh_bu_parse()
When acting as HA, the BU handler (ha_recv_bu()) handles both kinds of
BU, i.e. w/ and w/o the H flag set. This handler simply performs a
call to ha_recv_bu_main() which may pass the processing of the BU to
cn_recv_bu() if H flag is not set.

In both functions (cn_recv_bu() and ha_recv_bu_main()), mh_bu_parse()
is called. That function contains both parsing and checking parts which
are specific to HA and CN cases. For instance, the return path on the
HA ends up in the middle of the function (i.e. the end of the function
is dedicated to CN checks).

This patch:
- simplifies mh_bu_parse() in order to keep only the code common to
  all cases, i.e. the parsing code.
- extracts HA and CN specific checks in two helpers:
  mh_ha_home_bu_check() and mh_cn_bu_check().
- change ha_recv_bu() (the main MH BU handler on the HA) to perform
  the dispatch between HA and CN processing earlier.
- renamed ha_recv_bu_main() to ha_recv_home_bu()
- perform the required change in vt.c where ha_recv_home_bu() is also
  called (i.e. now do CN/HA dispatch)
- move HA and CN specific code to ha.c and cn.c

src/cn.c
@@ -47,6 +47,14 @@
 #include "cn.h"
 #include "conf.h"
 
+#define MH_DEBUG_LEVEL 1
+
+#if MH_DEBUG_LEVEL >= 1
+#define MDBG dbg
+#else
+#define MDBG(x...)
+#endif
+
 #define ICMP_ERROR_PERSISTENT_THRESHOLD 3
 
 const struct timespec cn_brr_before_expiry_ts =
@@ -149,6 +157,47 @@ static struct mh_handler cn_coti_handler = {
 	.recv = cn_recv_coti,
 };
 
+/* After parsing, perform CN-specific checks (auth + nonce) on BU */
+static int cn_bu_check(struct ip6_mh_binding_update *bu, ssize_t len,
+		       struct mh_options *mh_opts,
+		       struct in6_addr *peer_addr,
+		       struct in6_addr *our_addr,
+		       struct in6_addr *bind_coa,
+		       struct timespec *lifetime, uint8_t *key)
+{
+	struct ip6_mh_opt_nonce_index *non_ind;
+	struct ip6_mh_opt_auth_data *bauth;
+	int ret;
+
+	non_ind = mh_opt(&bu->ip6mhbu_hdr, mh_opts, IP6_MHOPT_NONCEID);
+	if (!non_ind)
+		return -1;
+
+	MDBG("src %x:%x:%x:%x:%x:%x:%x:%x\n", NIP6ADDR(peer_addr));
+	MDBG("coa %x:%x:%x:%x:%x:%x:%x:%x\n", NIP6ADDR(bind_coa));
+
+	if (tsisset(*lifetime))
+		ret = rr_cn_calc_Kbm(ntohs(non_ind->ip6moni_home_nonce),
+				     ntohs(non_ind->ip6moni_coa_nonce),
+				     peer_addr, bind_coa, key);
+	else /* Only use home nonce and address for dereg. */
+		ret = rr_cn_calc_Kbm(ntohs(non_ind->ip6moni_home_nonce), 0,
+				     peer_addr, NULL, key);
+	if (ret)
+		return ret;
+
+	bauth = mh_opt(&bu->ip6mhbu_hdr, mh_opts, IP6_MHOPT_BAUTH);
+	if (!bauth)
+		return -1;
+
+	/* Authenticator is calculated with MH checksum set to 0 */
+	bu->ip6mhbu_hdr.ip6mh_cksum = 0;
+	if (mh_verify_auth_data(bu, len, bauth, bind_coa, our_addr, key) < 0)
+		return -1;
+
+	return IP6_MH_BAS_ACCEPTED;
+}
+
 void cn_recv_bu(const struct ip6_mh *mh, ssize_t len,
 		const struct in6_addr_bundle *in, int iif)
 {
@@ -165,8 +214,11 @@ void cn_recv_bu(const struct ip6_mh *mh, ssize_t len,
 
 	bu = (struct ip6_mh_binding_update *)mh;
 
-	status = mh_bu_parse(bu, len, in, &out, &mh_opts, &lft, key);
-	if (status < 0)
+	if (mh_bu_parse(bu, len, in, &out, &mh_opts, &lft) < 0)
+		return;
+
+	if ((status = cn_bu_check(bu, len, &mh_opts, out.dst, out.src,
+				  out.bind_coa, &lft, key)) < 0)
 		return;
 
 	seqno = ntohs(bu->ip6mhbu_seqno);

src/ha.c
@@ -1126,24 +1126,37 @@ send_nack:
 	goto out;
 }
 
-int ha_recv_bu_main(const struct ip6_mh *mh, ssize_t len,
-		    const struct in6_addr_bundle *in, int iif, uint32_t flags)
+/* After parsing, perform HA-specific checks on *home* bu */
+static inline int ha_home_bu_check(struct ip6_mh_binding_update *bu,
+			      struct mh_options *mh_opts)
 {
-	struct ip6_mh_binding_update *bu;
+	if (!(bu->ip6mhbu_flags & IP6_MH_BU_HOME) ||
+	    mh_opt(&bu->ip6mhbu_hdr, mh_opts, IP6_MHOPT_NONCEID) != NULL ||
+	    mh_opt(&bu->ip6mhbu_hdr, mh_opts, IP6_MHOPT_BAUTH) != NULL)
+		return -1;
+
+	return IP6_MH_BAS_ACCEPTED;
+}
+
+/* Handle *Home* BU on HA (dispatch for received BU is done in BU handler) */
+int ha_recv_home_bu(const struct ip6_mh *mh, ssize_t len,
+		    const struct in6_addr_bundle *in, int iif,
+		    uint32_t flags)
+{
 	struct mh_options mh_opts;
 	struct in6_addr_bundle out;
 	struct ha_recv_bu_args *arg;
+	struct ip6_mh_binding_update *bu;
 	struct timespec lft;
-	int status = 0;
+	int status;
 	pthread_t worker;
 
 	bu = (struct ip6_mh_binding_update *)mh;
 
-	if (!(bu->ip6mhbu_flags & IP6_MH_BU_HOME)) {
-		cn_recv_bu(mh, len, in, iif);
-		return 0;
-	}
-	if (mh_bu_parse(bu, len, in, &out, &mh_opts, &lft, NULL) < 0)
+	if (mh_bu_parse(bu, len, in, &out, &mh_opts, &lft) < 0)
+		return -EINVAL;
+
+	if ((status = ha_home_bu_check(bu, &mh_opts)) < 0)
 		return -EINVAL;
 
 	arg = malloc(sizeof(struct ha_recv_bu_args) + len);
@@ -1198,7 +1211,12 @@ int ha_recv_bu_main(const struct ip6_mh *mh, ssize_t len,
 static void ha_recv_bu(const struct ip6_mh *mh, ssize_t len,
 		       const struct in6_addr_bundle *in, int iif)
 {
-	(void)ha_recv_bu_main(mh, len, in, iif, 0);
+	struct ip6_mh_binding_update *bu = (struct ip6_mh_binding_update *)mh;
+
+	if (bu->ip6mhbu_flags & IP6_MH_BU_HOME)
+		(void)ha_recv_home_bu(mh, len, in, iif, 0);
+	else
+		(void)cn_recv_bu(mh, len, in, iif);
 }
 
 static struct mh_handler ha_bu_handler = {

src/ha.h
@@ -42,7 +42,7 @@ struct ha_interface *ha_get_if(int ifindex);
 struct ha_interface *ha_get_if_by_addr(const struct in6_addr *addr);
 struct ha_interface *ha_get_if_by_anycast(const struct in6_addr *anycast,
 					  struct in6_addr **addr);
-int ha_recv_bu_main(const struct ip6_mh *mh, ssize_t len,
+int ha_recv_home_bu(const struct ip6_mh *mh, ssize_t len,
 		    const struct in6_addr_bundle *in, int iif, uint32_t flags);
 #define HA_BU_F_THREAD_JOIN	0x01
 #define HA_BU_F_PASSIVE_SEQ	0x02

src/mh.c
@@ -1016,18 +1016,17 @@ void mh_send_ba(const struct in6_addr_bundle *addrs, uint8_t status,
 	free_iov_data(mh_vec, iovlen);
 }
 
+/* Main BU parser, used by both HA (H flag set) and CN (H flag not set).
+ * Additional specific checks are performed for HA and CN via two
+ * specific helpers: ha_bu_check() (ha.c) and cn_bu_check() (cn.c). */
 int mh_bu_parse(struct ip6_mh_binding_update *bu, ssize_t len,
 		const struct in6_addr_bundle *in_addrs,
 		struct in6_addr_bundle *out_addrs,
 		struct mh_options *mh_opts,
-		struct timespec *lifetime, uint8_t *key)
+		struct timespec *lifetime)
 {
 	struct in6_addr *our_addr, *peer_addr, *remote_coa;
 	struct ip6_mh_opt_altcoa *alt_coa;
-	struct ip6_mh_opt_nonce_index *non_ind;
-	struct ip6_mh_opt_auth_data *bauth;
-	uint16_t bu_flags;
-	int ret;
 
 	MDBG("Binding Update Received\n");
 	if (len < sizeof(struct ip6_mh_binding_update) ||
@@ -1068,43 +1067,11 @@ int mh_bu_parse(struct ip6_mh_binding_update *bu, ssize_t len,
 	if (!out_addrs->bind_coa)
 		out_addrs->bind_coa = in_addrs->src;
 
-	bu_flags = bu->ip6mhbu_flags;
 	out_addrs->src = in_addrs->dst;
 	out_addrs->dst = in_addrs->src;
 	out_addrs->local_coa = NULL;
 
-	non_ind = mh_opt(&bu->ip6mhbu_hdr, mh_opts, IP6_MHOPT_NONCEID);
-
-	if (bu_flags & IP6_MH_BU_HOME)
-		return non_ind ? -1 : 0;
-
-	if (!non_ind)
-		return -1;
-
-	MDBG("src %x:%x:%x:%x:%x:%x:%x:%x\n", NIP6ADDR(peer_addr));
-	MDBG("coa %x:%x:%x:%x:%x:%x:%x:%x\n", 
-	     NIP6ADDR(out_addrs->bind_coa));
-
-	if (tsisset(*lifetime))
-		ret = rr_cn_calc_Kbm(ntohs(non_ind->ip6moni_home_nonce),
-				     ntohs(non_ind->ip6moni_coa_nonce),
-				     peer_addr, out_addrs->bind_coa, key);
-	else /* Only use home nonce and address for dereg. */
-		ret = rr_cn_calc_Kbm(ntohs(non_ind->ip6moni_home_nonce), 0,
-				     peer_addr, NULL, key);
-	if (ret)
-		return ret;
-
-	bauth = mh_opt(&bu->ip6mhbu_hdr, mh_opts, IP6_MHOPT_BAUTH);
-	if (!bauth)
-		return -1;
-	/* Authenticator is calculated with MH checksum set to 0 */
-	bu->ip6mhbu_hdr.ip6mh_cksum = 0;
-	if (mh_verify_auth_data(bu, len, bauth, 
-				out_addrs->bind_coa, our_addr, key) < 0)
-		return -1;
-	
-	return IP6_MH_BAS_ACCEPTED;
+	return 0;
 }
 
 void mh_cleanup(void)

src/mh.h
@@ -123,8 +123,7 @@ int mh_bu_parse(struct ip6_mh_binding_update *bu, ssize_t len,
 		const struct in6_addr_bundle *in_addrs,
 		struct in6_addr_bundle *out_addrs,
 		struct mh_options *mh_opts,
-		struct timespec *lifetime,
-		uint8_t *key);
+		struct timespec *lifetime);
 
 void mh_handler_reg(uint8_t type, struct mh_handler *handler);
 void mh_handler_dereg(uint8_t type, struct mh_handler *handler);

src/vt.c
@@ -61,6 +61,7 @@
 #include "bcache.h"
 #include "prefix.h"
 #include "ha.h"
+#include "cn.h"
 #include "mn.h"
 #include "mpdisc_mn.h"
 #include "mpdisc_ha.h"
@@ -904,6 +905,8 @@ static int bcache_vt_cmd_bc_mod(const struct vt_handle *vh, const char *str,
 	{
 		struct ip6_mh_binding_update bu;
 		struct in6_addr_bundle ab;
+		struct ip6_mh *mh = (struct ip6_mh *)&bu;
+		int len = sizeof(bu);
 
 		memset(&bu, 0, sizeof(bu));
 		bu.ip6mhbu_flags = bce_flags;
@@ -918,8 +921,12 @@ static int bcache_vt_cmd_bc_mod(const struct vt_handle *vh, const char *str,
 			ab.remote_coa = NULL;
 		ab.bind_coa = NULL;
 
-		err = ha_recv_bu_main((struct ip6_mh *)&bu, sizeof(bu), &ab, 0,
-				      flags);
+		if (bu.ip6mhbu_flags & IP6_MH_BU_HOME)
+			err = ha_recv_home_bu(mh, len, &ab, 0, flags);
+		else {
+			cn_recv_bu(mh, len, &ab, 0);
+			err = 0;
+		}
 	}
 	if (err < 0)
 		fprintf(vh->vh_stream, "bc error=%d(%s)\n",

commit b9ea7fd6d6f94f21fb125be772e810b8b866bd88

Handle BU processing regression for CN introduced by 36c929e1
While extracting CN and HA checks of mh_bu_parse() in commit 36c929e1,
checks for H flag in BU were omitted in CN code path. As reported, this
caused TAHI correspondent node tests 118-123 (5.3. Receiving BU with (H)
bit is set) to fail.

This commit restores the behavior expected by the RFC and TAHI tests.
It also makes associated code more readable. The whole thread is here:

  http://thread.gmane.org/gmane.network.ipv6.nautilus6.general/477

Reported-by: Steve Chen <schen@mvista.com>

src/cn.c
@@ -167,7 +167,9 @@ static struct mh_handler cn_coti_handler = {
 	.recv = cn_recv_coti,
 };
 
-/* After parsing, perform CN-specific checks (auth + nonce) on BU */
+/* After parsing, perform CN-specific checks (flags and auth + nonce) on BU.
+ * -1 is returned if the BU needs to be silently dropped. Otherwise, a BA
+ * status code is returned. If it is an error code, the BU should be dropped */
 static int cn_bu_check(struct ip6_mh_binding_update *bu, ssize_t len,
 		       struct mh_options *mh_opts,
 		       struct in6_addr *peer_addr,
@@ -180,8 +182,31 @@ static int cn_bu_check(struct ip6_mh_binding_update *bu, ssize_t len,
 	int ret;
 
 	non_ind = mh_opt(&bu->ip6mhbu_hdr, mh_opts, IP6_MHOPT_NONCEID);
+
+	if (bu->ip6mhbu_flags & IP6_MH_BU_HOME) {
+		if (non_ind) {
+			/* BU w/ Nonce and H bit set. It is simply invalid.
+			 * Drop it. This matches behavior expectedin TAHI
+			 * CN tests: 5-3-4, 5-3-5, 5-3-6 */
+			return -1;
+		}
+		/* Now, as a CN, we are receiving a BU intended for a HA.
+		 * We will send a BA to report the error. If the peer is
+		 * already registered: registration type change is not
+		 * allowed (TAHI CN tests 5-3-2, 5-3-3). If peer is unknown
+		 * to us, just warn we are not a HA (TAHI CN Test 5-3-1) */
+		if (bce_exists(our_addr, peer_addr))
+			return IP6_MH_BAS_REG_NOT_ALLOWED;
+		else
+			return IP6_MH_BAS_HA_NOT_SUPPORTED;
+	}
+
 	if (!non_ind)
 		return -1;
+
+	/* We could also test if BU has R flag set (NEMO) and drop
+	 * it in that case. We just act as expected by RFC 3775, i.e.
+	 * just don't consider the value of the flag --arno */
 
 	MDBG("src %x:%x:%x:%x:%x:%x:%x:%x\n", NIP6ADDR(peer_addr));
 	MDBG("coa %x:%x:%x:%x:%x:%x:%x:%x\n", NIP6ADDR(bind_coa));
@@ -241,13 +266,6 @@ void cn_recv_bu(const struct ip6_mh *mh, ssize_t len,
 	non_ind = mh_opt(&bu->ip6mhbu_hdr, &mh_opts, IP6_MHOPT_NONCEID);
 	bce = bcache_get(out.src, out.dst);
 	if (bce) {
-		if ((bce->flags^bu_flags) & (IP6_MH_BU_HOME|IP6_MH_BU_MR)) {
-			/* H-bit or R-bit mismatch, flags changed */
-			bcache_release_entry(bce);
-			bce = NULL;
-			status = IP6_MH_BAS_REG_NOT_ALLOWED;
-			goto send_nack;
-		}
 		if (!MIP6_SEQ_GT(seqno, bce->seqno)) {
 			uint16_t nonce_hoa;
 			if (bce->type != BCE_NONCE_BLOCK) {
@@ -264,8 +282,7 @@ void cn_recv_bu(const struct ip6_mh *mh, ssize_t len,
 			 * have been used before with the same home address.
 			 * This is more strict than the draft, but otherwise
 			 * we would need to store all non-expired home kgen
-			 * tokens mn had used before deregistration.
-			 */
+			 * tokens mn had used before deregistration. */
 			nonce_hoa = ntohs(non_ind->ip6moni_home_nonce);
 			if (bce->nonce_hoa == nonce_hoa) {
 				bcache_release_entry(bce);
@@ -285,16 +302,8 @@ void cn_recv_bu(const struct ip6_mh *mh, ssize_t len,
 			/* else get rid of it */
 			bcache_delete(out.src, out.dst);
 		}
-	} else {
-		if (bu_flags & IP6_MH_BU_HOME) {
-			status = IP6_MH_BAS_HA_NOT_SUPPORTED;
-			goto send_nack;
-		}
-		if (bu_flags & IP6_MH_BU_MR) {
-			status = IP6_MH_BAS_MR_OP_NOT_PERMITTED;
-			goto send_nack;
-		}
 	}
+
 	status = conf.pmgr.discard_binding(out.dst, out.bind_coa,
 					   out.src, bu, len);
 